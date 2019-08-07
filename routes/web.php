fac574f986b3f9490b4ee66be1639ad5720a54b9
rasmus0201 <xxr@live.dk>
2019-08-07 10:00:40 +0200

    update stuff

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,3 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-#Disable authentication/user module
-#Auth::routes();
-
 Route::get('/', 'PortfolioController@index')->name('start');
