7fab0ff609495ba214315ac29d9bada2b176cce5
rasmus0201 <xxr@live.dk>
2019-10-14 13:20:34 +0200

    add dark mode

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
 import VueScrollTo from 'vue-scrollto';
 import EventBus from './event-bus';
+
+Vue.use(Vuex);
 
 Vue.use(VueScrollTo, {
     container: 'body',
@@ -19,6 +22,12 @@ Vue.mixin({
     }
 });
 
+/**
+ * Store
+ */
+import store from './store/store';
+
+
 require('./bootstrap');
 require('./interactive-background');
 require('./directives/track');
@@ -33,29 +42,88 @@ require('./directives/track');
     files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 }
 
+import { mapState, mapActions } from 'vuex';
+
 const app = new Vue({
+    store: store,
+
+    data() {
+        return {
+            easterEgg: {
+                cursor: 0,
+                code: 'dark', // ['d', 'a', 'r', 'k'],
+            }
+        };
+    },
+
+    computed: {
+        ...mapState({
+            darkMode: state => state.darkMode.enabled
+        })
+    },
+
+    watch: {
+        darkMode: function (newValue, oldValue) {
+            if (newValue === true) {
+                document.body.classList.add('dark-mode');
+            } else {
+                document.body.classList.remove('dark-mode');
+            }
+        }
+    },
+
+    created() {
+        if (localStorage.getItem('darkMode') === 'true') {
+            this.toggleDarkMode();
+        }
+    },
+
     mounted() {
         this.$el.classList.remove('v-hide');
         this.maybeUpdateActiveNavItem();
         this.maybeUpdateNavScrollbarVisibility();
+
+        window.addEventListener('keyup', this.maybeActivateEasterEgg);
 
         window.addEventListener('keyup', this.maybeCloseNav);
         window.addEventListener('scroll', this.maybeUpdateActiveNavItem);
         window.addEventListener('resize', this.maybeUpdateActiveNavItem);
         window.addEventListener('resize', this.maybeUpdateNavScrollbarVisibility);
 
-        console.log('👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋');
-        console.log('Nysgerrig?');
-        console.log('Skriv til mig på bundsgaard.rasmus@gmail.com');
-        console.log('----');
-        console.log('Ver: 2.0.0');
-        console.log('Frontend: Vue2, FontAwesome 5, Bootstrap 4, Particles.js, Google Fonts');
-        console.log('Backend: NGINX, PHP7, Laravel');
+        // console.log('👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋');
+        // console.log('Nysgerrig?');
+        // console.log('Skriv til mig på bundsgaard.rasmus@gmail.com');
+        // console.log('----');
+        // console.log('Ver: 2.0.0');
+        // console.log('Frontend: Vue2, FontAwesome 5, Bootstrap 4, Particles.js, Google Fonts');
+        // console.log('Backend: NGINX, PHP7, Laravel');
     },
+
     methods: {
+        ...mapActions({
+            toggleDarkMode: 'darkMode/toggle'
+        }),
+
         maybeCloseNav(event) {
             if (event.keyCode == 27) {
                 EventBus.$emit('navigation:close');
+            }
+        },
+
+        maybeActivateEasterEgg(e) {
+            // If not the typed char is not the next character
+            // then reset the cursor
+            if (this.easterEgg.code[this.easterEgg.cursor] !== e.key) {
+                this.easterEgg.cursor = 0;
+
+                return;
+            }
+
+            // Check if last character of code
+            if (++this.easterEgg.cursor == this.easterEgg.code.length) {
+                this.toggleDarkMode();
+
+                this.easterEgg.cursor = 0;
             }
         },
 
