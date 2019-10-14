c6803cdc0a928f68ebad252bec44c617cbbb0243
rasmus0201 <xxr@live.dk>
2019-10-14 14:39:39 +0200

    more data driven

--- resources/assets/js/sections/BundsgaardSkills.vue
+++ resources/assets/js/sections/BundsgaardSkills.vue
@@ -3,75 +3,100 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12 text-center text-uppercase">
-                    <h1 class="display-3 mb-3">Kompetencer</h1>
+                    <h1 class="display-3 mb-3">{{ title }}</h1>
                 </div>
             </div>
-            <div class="row mb-5">
+
+            <div class="row mb-5" v-for="category in categories" :key="category.title">
                 <div class="col-md-1 d-flex justify-content-center align-items-center">
-                    <span class="skillset-type">Backend</span>
+                    <span class="skillset-type">{{ category.title }}</span>
                 </div>
                 <div class="col-md-6">
-                    <div class="progress progress-md mb-3">
-                        <div class="progress-bar bg-success" role="progressbar" style="width: 95%" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">
-                            PHP 5/7
-                        </div>
-                    </div>
-                    <div class="progress progress-md mb-3">
-                        <div class="progress-bar bg-success" role="progressbar" style="width: 90%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
-                            Laravel 5
-                        </div>
-                    </div>
-                    <div class="progress progress-md mb-3">
-                        <div class="progress-bar bg-success" role="progressbar" style="width: 90%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
-                            <span class="d-none d-xl-block">Wordpress. Opsætning, Tema og Plugin-udvikling.</span>
-                            <span class="d-xl-none">Wordpress</span>
-                        </div>
-                    </div>
-                    <div class="progress progress-md mb-3">
-                        <div class="progress-bar bg-success" role="progressbar" style="width: 70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="30">
-                            Linux
+                    <div class="progress progress-md mb-3" v-for="(skill, index) in category.skills" :key=" category.title + '-' + index">
+                        <div
+                            class="progress-bar"
+                            :class="[skill.class]"
+                            role="progressbar"
+                            :style="{ 'width' : skill.experience + '%' }"
+                            :aria-valuenow="skill.experience"
+                            aria-valuemin="0"
+                            aria-valuemax="100"
+                            v-html="skill.body">
                         </div>
                     </div>
                 </div>
-                <div class="col-md-5">
-                    Jeg arbejder bedst i PHP med Laravel som Framework eller Wordpress CMS. Jeg har også arbejdet med versionsstyring (Git). Jeg synes det er sjovt at lære nye teknologier, og vil gerne lære at lave Apps, eller Desktop Software. Jeg har programmeret i PHP siden starten af 2014.
-                    <br><br>
-                    Jeg har også god erfaring med Linux. Denne side er hostet på VPS hos Digital Ocean med Ubuntu Server.
-                </div>
-            </div>
-            <div class="row mb-5">
-                <div class="col-md-1 d-flex justify-content-center align-items-center">
-                    <span class="skillset-type">Design</span>
-                </div>
-                <div class="col-md-6">
-                    <div class="progress progress-md mb-3">
-                        <div class="progress-bar bg-success" role="progressbar" style="width: 99%" aria-valuenow="99" aria-valuemin="0" aria-valuemax="100">
-                            HTML5
-                        </div>
-                    </div>
-                    <div class="progress progress-md mb-3">
-                        <div class="progress-bar bg-success" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
-                            CSS3/4
-                        </div>
-                    </div>
-                    <div class="progress progress-md mb-3">
-                        <div class="progress-bar bg-success" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
-                            Bootstrap 3/4
-                        </div>
-                    </div>
-                    <div class="progress progress-md mb-3">
-                        <div class="progress-bar bg-warning" role="progressbar" style="width: 60%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
-                            Photoshop CC
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-5">
-                    Jeg har arbejdet med HTML/CSS mere end {{ Bundsgaard.yop }} år, og jeg har holdt mig up-to-date med de nyeste tags og CSS-regler. Jeg er dog ikke en UI/UX Designer / Grafiker, og er meget mere interesseret i at arbejde i det bagvedliggende kode. Det er dermed ikke sagt jeg ikke kan, men det er bare ikke en spidskompetence, jeg kan også sagtens følge en skitse/ et design ned til mindste detalje. Jeg har også arbejdet lidt Adobe programmerne, samt noget 3D-modellering.
-                </div>
+                <div class="col-md-5" v-html="category.description"></div>
             </div>
         </div>
     </section>
 </template>
 <script>
-    export default {}
+    export default {
+        data() {
+            return {
+                title: 'Kompetencer',
+                categories: [
+                    {
+                        title: 'Backend',
+                        description: `Jeg arbejder bedst i PHP med Laravel som Framework eller Wordpress CMS. Jeg har også arbejdet med versionsstyring (Git). Jeg synes det er sjovt at lære nye teknologier, og vil gerne lære at lave Apps, eller Desktop Software. Jeg har programmeret i PHP siden starten af 2014.
+                        <br><br>
+                        Jeg har også god erfaring med Linux. Denne side er hostet på VPS hos Digital Ocean med Ubuntu Server.`,
+                        skills: [
+                            {
+                                body: 'PHP 5/7',
+                                experience: 95,
+                                class: 'bg-success'
+                            },
+                            {
+                                body: 'Laravel 5',
+                                experience: 93,
+                                class: 'bg-success'
+                            },
+                            {
+                                body: '<span class="d-none d-xl-block">Wordpress. Opsætning, Tema og Plugin-udvikling.</span><span class="d-xl-none">Wordpress</span>',
+                                experience: 88,
+                                class: 'bg-success'
+                            },
+                            {
+                                body: 'Linux',
+                                experience: 83,
+                                class: 'bg-success'
+                            }
+                        ]
+                    },
+                    {
+                        title: 'Frontend',
+                        description: `Jeg har arbejdet med HTML/CSS mere end ${Bundsgaard.yop} år, og jeg har holdt mig up-to-date med de nyeste tags og CSS-regler. Jeg kan følge en skitse/ et design ned til mindste detalje. Jeg har prøvet kræfter med JS frameworks som VueJS, Angular og AngularJS. Jeg har også arbejdet lidt Adobe programmerne, samt noget 3D-modellering.`,
+                        skills: [
+                            {
+                                body: 'HTML5',
+                                experience: 99,
+                                class: 'bg-success'
+                            },
+                            {
+                                body: 'CSS3/4',
+                                experience: 92,
+                                class: 'bg-success'
+                            },
+                            {
+                                body: 'Javascript (ES5 & ES6)',
+                                experience: 87,
+                                class: 'bg-success'
+                            },
+                            {
+                                body: 'Bootstrap 4',
+                                experience: 92,
+                                class: 'bg-success'
+                            },
+                            {
+                                body: 'Photoshop CC',
+                                experience: 60,
+                                class: 'bg-warning'
+                            },
+                        ]
+                    },
+                ]
+            };
+        }
+    }
 </script>
--- resources/assets/js/sections/BundsgaardStart.vue
+++ resources/assets/js/sections/BundsgaardStart.vue
@@ -6,8 +6,8 @@
             <div class="row">
                 <div class="col-12 text-center text-white">
                     <h1 class="display-3 text-uppercase">{{ name }}</h1>
-                    <h4>Mit online portfolio og CV</h4>
-                    <p>Her finder du tidligere opgaver, erfaringer, uddannelse og oplysninger om mig.</p>
+                    <h4>{{ title }}</h4>
+                    <p>{{ subtitle }}</p>
                     <div class="row mt-4">
                         <div class="contact-option col-12 text-center">
                             <b-button tag="a" :rounded="true" v-gclick="'contact:inbound'" class="px-4 mr-2 mb-2 mb-lg-0" :href="'mailto:' + Bundsgaard.social.mail">
@@ -27,6 +27,13 @@
 </template>
 <script>
 export default {
+    data() {
+        return {
+            title: 'Mit online portfolio og CV',
+            subtitle: 'Her finder du tidligere opgaver, erfaringer, uddannelse og oplysninger om mig.'
+        };
+    },
+
     computed: {
         name: function() {
             return Bundsgaard.firstName + ' ' + Bundsgaard.middleName;
